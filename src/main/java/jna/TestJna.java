3be6a5081d38d1752d5d6fa62f97113c0ed5b881
rateyu <rateyu@gmail.com>
2017-06-21 23:06:03 +0800

    jna 恢复 spring boot恢复

--- src/main/java/jna/TestJna.java
+++ src/main/java/jna/TestJna.java
@@ -1,24 +1,24 @@
-package jna;
-
-import com.sun.jna.Library;
-import com.sun.jna.Native;
-
-public class TestJna {
-    public interface LgetLib extends Library {
-        // 调用linux下面的so文件,注意，这里只要写test就可以了，不要写libtest，也不要加后缀
-        // LgetLib INSTANCE = (LgetLib) Native.loadLibrary("testShareLib",LgetLib.class);
-        //
-        LgetLib INSTANCE = (LgetLib) Native.loadLibrary("testStaticLib",LgetLib.class);
-        int SampleAddInt(int a,int b);
-    }
-
-    public int SampleAddInt(int a,int b){
-        return LgetLib.INSTANCE.SampleAddInt(a,b);
-    }
-
-    public static void main(String[] args) {
-        TestJna ts = new TestJna();
-        int c = ts.SampleAddInt(10,20);
-        System.out.println("10+20="+c);
-    }
-}
+//package jna;
+//
+//import com.sun.jna.Library;
+//import com.sun.jna.Native;
+//
+//public class TestJna {
+//    public interface LgetLib extends Library {
+//        // 调用linux下面的so文件,注意，这里只要写test就可以了，不要写libtest，也不要加后缀
+//        // LgetLib INSTANCE = (LgetLib) Native.loadLibrary("testShareLib",LgetLib.class);
+//        //
+//        LgetLib INSTANCE = (LgetLib) Native.loadLibrary("testStaticLib",LgetLib.class);
+//        int SampleAddInt(int a,int b);
+//    }
+//
+//    public int SampleAddInt(int a,int b){
+//        return LgetLib.INSTANCE.SampleAddInt(a,b);
+//    }
+//
+//    public static void main(String[] args) {
+//        TestJna ts = new TestJna();
+//        int c = ts.SampleAddInt(10,20);
+//        System.out.println("10+20="+c);
+//    }
+//}
